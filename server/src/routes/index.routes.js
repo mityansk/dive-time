59ccd6794ba04163526b63ab0e75f5ff09068b71
mityansk <retest@gmail.com>
2025-03-12 14:56:02 +0300

    init commit

---
+++ server/src/routes/index.routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const authRoutes = require('./auth.routes');
+const formatResponse = require('../utils/formatResponse');
+
+router.use('/auth', authRoutes);
+// router.use('/posts', postRoutes);
+
+router.use('*', (req, res) => {
+  res.status(404).json(formatResponse(404, 'Not Found!'));
+});
+
+module.exports = router;
