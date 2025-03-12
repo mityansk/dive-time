323c59d5532a357e3a67bba02d234a8e21c40174
Niyaz <moranpro86@gmail.com>
2025-03-12 16:55:33 +0300

    route service controller created and workong

--- server/src/routes/tour.routes.js
+++ server/src/routes/tour.routes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
+const TourController = require("../controllers/Tour.Controller");
+const verifyAccessToken = require("../middleware/verifyAccessToken");
 
 router.get('/', TourController.getAllTour)
 router.get('/:id', TourController.getTourById)
-router.post('/', TourController.createTour)
-router.put('/:id', TourController.UpdateTour)
-router.delete('/:id', TourController.deleteTour)
+router.post('/', verifyAccessToken, TourController.createTour)
+router.put('/:id', verifyAccessToken, TourController.UpdateTour)
+router.delete('/:id', verifyAccessToken, TourController.deleteTour)
 
 module.exports = router
