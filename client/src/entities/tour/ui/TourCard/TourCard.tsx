41ecb7bdb8d77bd884873c0cd203cb49bf101a7d
Niyaz <moranpro86@gmail.com>
2025-03-13 20:12:32 +0300

    updateAlready working commit before change date

--- client/src/entities/tour/ui/TourCard/TourCard.tsx
+++ client/src/entities/tour/ui/TourCard/TourCard.tsx
@@ -1,34 +1,41 @@
-import { JSX, useState } from 'react'
-import { ITour } from '../../model'
-import styles from './TourCard.module.css'
-import TourUpdateForm from '../TourUpdateForm/TourUpdateForm'
+import { JSX, useState } from 'react';
+import { ITour } from '../../model';
+import styles from './TourCard.module.css';
+import TourUpdateForm from '../TourUpdateForm/TourUpdateForm';
 
 interface TourCardProps {
-	tour: ITour
-	onClick: () => void
+  tour: ITour;
+  onClick: () => void;
 }
 
-export default function TourCard({ tour, onClick }: TourCardProps): JSX.Element {
-	const { location_name, description, date } = tour
-	// const author = tour.author.username
+export default function TourCard({
+  tour,
+  onClick,
+}: TourCardProps): JSX.Element {
+  const { location_name, description, date } = tour;
+  //! const author = tour.author.username
 
-	const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-	  const handleSave = () => {
-      setIsEditing(false); 
-    };
-	return    ( <>
+  const handleSave = () => {
+    setIsEditing(false);
+  };
+  return (
+    <>
       {isEditing ? (
         <TourUpdateForm tour={tour} onSave={handleSave} />
       ) : (
         <div className={styles.card} onClick={onClick}>
-          <span className={styles.location}>Локация: {tour.location_name}</span>
-          <span className={styles.description}>Описание: {tour.description}</span>
-          <span className={styles.date}>Дата: {tour.date}</span>
+          <span className={styles.location}>Локация: {location_name}</span>
+          <span className={styles.description}>
+            Описание: {description}
+          </span>
+          <span className={styles.date}>Дата: {date}</span>
+          {/* <span>Автор тура: {author}</span> */}
           <span className={styles.author}>Автор тура: Pupkin</span>
           <button
             onClick={(e) => {
-              e.stopPropagation(); 
+              e.stopPropagation();
               setIsEditing(true);
             }}
             className={styles.editButton}
