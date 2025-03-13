4db9fd5837fce9a0fa75ec8cf9e60f6be15ae0f7
Artem-Dubaev <artem3110dub@icloud.com>
2025-03-13 17:28:46 +0500

    add map

--- client/src/pages/LocationPage/LocationPage.tsx
+++ client/src/pages/LocationPage/LocationPage.tsx
@@ -1,3 +1,4 @@
+import { YMaps, Map } from '@pbe/react-yandex-maps';
 import { useAppDispatch, useAppSelector } from '@/shared/hooks/reduxHooks';
 import styles from './LocationPage.module.css';
 import { getLocation } from '@/entities/location';
@@ -13,18 +14,31 @@ export function LocationPage() {
 		dispatch(getLocation());
 	}, [dispatch]);
 
-	return (
-		<div className={styles.container}>
-			<div className={styles.map}>КАРТА</div>
-			<div className={styles.box}>
-				<h2>Места дайвинга</h2>
+const CENTER = [51.84168211156664, 104.76153977184012];
+const ZOOM = 12
 
-				{state?.map(location => (
-					<Link to={`/locations/${location.id}`} className={styles.card}>
-						<LocationCard key={location.id} location={location} />
-					</Link>
-				))}
+	return (
+		<YMaps>
+			<div className={styles.container}>
+				<div className={styles.map}>
+					<Map
+						className={styles.mapDisplay}
+						defaultState={{
+							center: CENTER,
+							zoom: ZOOM,
+						}}
+					/>
+				</div>
+				<div className={styles.box}>
+					<h2>Места дайвинга</h2>
+
+					{state?.map(location => (
+						<Link to={`/locations/${location.id}`} className={styles.card}>
+							<LocationCard key={location.id} location={location} />
+						</Link>
+					))}
+				</div>
 			</div>
-		</div>
+		</YMaps>
 	);
 }
