14f5bc9fe9254193261e8036e69be36efca5a420
GMGinIT <gmginit@gmail.com>
2025-03-13 15:58:04 +0300

    hotfix

---
+++ client/src/app/layout/Layout.tsx
@@ -0,0 +1,22 @@
+import { refreshTokensThunk } from '@/entities/user/api';
+import { useAppDispatch } from '@/shared/hooks/reduxHooks';
+import Footer from '@/widgets/Footer/Footer';
+import Header from '@/widgets/Header/Header';
+import { ReactElement, useEffect } from 'react';
+import { Outlet } from 'react-router';
+
+export default function Layout(): ReactElement {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(refreshTokensThunk());
+  }, [dispatch]);
+
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
