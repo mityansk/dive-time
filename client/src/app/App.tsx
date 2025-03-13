714257652a574829d4d6a67b6d6912dd377703ad
mityansk <retest@gmail.com>
2025-03-13 19:18:28 +0300

    added boostrap styles

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,8 @@
 import { Provider } from "react-redux";
 import store from "./store/store";
 import Router from "./router/router";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
