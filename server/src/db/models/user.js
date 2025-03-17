cb8f6403980135764bbf8e7c96afee8abf6f0e06
mityansk <retest@gmail.com>
2025-03-17 22:05:49 +0300

    emailconfirmation almost done

--- server/src/db/models/user.js
+++ server/src/db/models/user.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
       username: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
+      isEmailConfirmed: DataTypes.BOOLEAN,
     },
     {
       sequelize,
