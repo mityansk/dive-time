4bab0fa81c9fe8dc7d7bf367618f965e50a5ab60
Niyaz <moranpro86@gmail.com>
2025-03-18 16:01:37 +0300

    prifile tour styles fixed

--- client/src/components/OneTourModal/OneTourModal.tsx
+++ client/src/components/OneTourModal/OneTourModal.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch, useAppSelector } from '@/shared/hooks/reduxHooks';
 import { getTourByIdThunk } from '@/entities/tour/api';
 import { Modal, Spin } from 'antd';
 import { useNavigate } from 'react-router';
+import styles from './OneTourModal.module.css'
 
 interface TourModalProps {
   tourId: number;
@@ -18,9 +19,8 @@ const OneTourModal: React.FC<TourModalProps> = ({
   const dispatch = useAppDispatch();
   const { one_tour, isLoading, error } = useAppSelector((state) => state.tour);
   const navigate = useNavigate();
-    const startDate = (one_tour?.start_date)
-    const endDate = (one_tour?.end_date)
-  
+  const startDate = one_tour?.start_date;
+  const endDate = one_tour?.end_date;
 
   useEffect(() => {
     if (isOpen && tourId) {
@@ -41,25 +41,31 @@ const OneTourModal: React.FC<TourModalProps> = ({
       open={isOpen}
       onCancel={onClose}
       footer={null}
+      className={styles.modalContent} 
     >
-      {isLoading && <Spin />}
-      {error && <div style={{ color: 'red' }}>{error}</div>}
+      {isLoading && (
+        <div className={styles.spinContainer}>
+          <Spin />
+        </div>
+      )}
+      {error && <div className={styles.errorMessage}>{error}</div>}
       {one_tour && (
         <div>
           <img
-            style={{
-              width: '472px',
-            }}
+            className={styles.modalImage}
             alt="Фотография карточки тура"
             src={`http://localhost:3000/${one_tour.image}`}
           />
-          <h2 onClick={handleLocationClick}>
-            {one_tour.location_name} ⬅ Узнать подробнее
+          <h2 className={styles.modalTitle} onClick={handleLocationClick}>
+            {one_tour.location_name}{' '}
+            <span className={styles.locationLink}>⬅ Узнать подробнее</span>
           </h2>
-          <p>{one_tour.description}</p>
-          <p>Дата начала тура: {startDate}</p>
-          <p>Дата конца тура: {endDate}</p>
-          <p>Автор: {one_tour.author.username}</p>
+          <p className={styles.modalDescription}>{one_tour.description}</p>
+          <p className={styles.modalDate}>Дата начала тура: {startDate}</p>
+          <p className={styles.modalDate}>Дата конца тура: {endDate}</p>
+          <p className={styles.modalAuthor}>
+            Автор: {one_tour.author.username}
+          </p>
         </div>
       )}
     </Modal>
